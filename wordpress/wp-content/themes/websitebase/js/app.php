e8d183f6b922178a80facb8a326042421d30018b
TriForceX <admin@gznetwork.com>
2017-07-07 17:05:54 -0400

    updated language JS in wordpress

--- wordpress/wp-content/themes/websitebase/js/app.php
+++ wordpress/wp-content/themes/websitebase/js/app.php
@@ -29,6 +29,9 @@ foreach($jsFiles as $jsFile){
 	$jsBuffer .= file_get_contents($jsFile);
 }
 
+$jsLanguage = array(
+				);
+
 $jsVariables = array(
 					//Screen
 					'@screen-small-phone' 	=> '320', 
@@ -40,31 +43,31 @@ $jsVariables = array(
 					'@screen-full-hd' 		=> '1920', 
 					//Global
 					'@global-url' => $jsUrl,
-					//Validation
-					'@validate-title' 			=> array('Form Alert', 'Alerta Formulario')[$jsLang], 
-					'@validate-normal' 			=> array('Please fill the fields.', 'Por favor complete los campos.')[$jsLang], 
-					'@validate-number'	 		=> array('Please type a valid number.', 'Por favor escriba un número válido.')[$jsLang], 
-					'@validate-tel' 			=> array('Please type a phone number.', 'Por favor escriba un teléfono válido.')[$jsLang], 
-					'@validate-pass' 			=> array('Please fill your password.', 'Por favor complete su clave.')[$jsLang], 
-					'@validate-email' 			=> array('Please type a correct E-Mail.', 'Por favor escriba un E-Mail válido.')[$jsLang],
-					'@validate-search' 			=> array('Please fill the search field.', 'Por favor complete el campo de busqueda.')[$jsLang], 
-					'@validate-checkbox' 		=> array('Please check an option.', 'Por favor elija una(s) opción(es).')[$jsLang],
-					'@validate-radio' 			=> array('Please check one of the options.')[$jsLang],
-					'@validate-textarea' 		=> array('Please write a message.', 'Por favor escriba un mensaje.')[$jsLang],
-					'@validate-select' 			=> array('Please select an option.' ,'Por favor seleccione una opción.')[$jsLang],
-					'@validate-confirm-title' 	=> array('Form Confirm')[$jsLang], 
-					'@validate-confirm-text' 	=> array('Are you sure you want to send the previous info?', '¿Estas seguro de que deseas enviar el formulario?')[$jsLang], 
+					//Form Validation
+					'@validate-title' 			=> $jsLang == 1 ? 'Alerta Formulario' :  'Form Alert', 
+					'@validate-normal' 			=> $jsLang == 1 ? 'Por favor complete los campos.' : 'Please fill the fields.', 
+					'@validate-number'	 		=> $jsLang == 1 ? 'Por favor escriba un número válido.' : 'Please type a valid number.', 
+					'@validate-tel' 			=> $jsLang == 1 ? 'Por favor escriba un teléfono válido.' : 'Please type a phone number.', 
+					'@validate-pass' 			=> $jsLang == 1 ? 'Por favor complete su clave.' : 'Please fill your password.', 
+					'@validate-email' 			=> $jsLang == 1 ? 'Por favor escriba un E-Mail válido.' : 'Please type a correct E-Mail.',
+					'@validate-search' 			=> $jsLang == 1 ? 'Por favor complete el campo de busqueda.' : 'Please fill the search field.', 
+					'@validate-checkbox' 		=> $jsLang == 1 ? 'Por favor elija opciones.' : 'Please check an option.',
+					'@validate-radio' 			=> $jsLang == 1 ? 'Por favor elija una de las opciones.' : 'Please check one of the options.',
+					'@validate-textarea' 		=> $jsLang == 1 ? 'Por favor escriba un mensaje.' : 'Please write a message.',
+					'@validate-select' 			=> $jsLang == 1 ? 'Por favor seleccione una opción.' : 'Please select an option.',
+					'@validate-confirm-title' 	=> $jsLang == 1 ? 'Confirmar Formulario' : 'Form Confirm', 
+					'@validate-confirm-text' 	=> $jsLang == 1 ? '¿Estas seguro de que deseas enviar el formulario?' : 'Are you sure you want to send the previous info?', 
 					//Video launch
-					'@videolaunch-title' 		=> array('Share Link', 'Compartir Enlace')[$jsLang], 
-					'@videolaunch-text' 		=> array('The share link has been copied!', '¡El enlace ha sido copiado!')[$jsLang],
+					'@videolaunch-title' 		=> $jsLang == 1 ? 'Compartir Enlace' : 'Share Link', 
+					'@videolaunch-text' 		=> $jsLang == 1 ? '¡El enlace ha sido copiado!' : 'The share link has been copied!',
 					//Map launch
-					'@maplaunch-title' 			=> array('Map Select', 'Mapa Dirección')[$jsLang],
-					'@maplaunch-text' 			=> array('Select one of options below', 'Seleccione una de las opciones')[$jsLang],
+					'@maplaunch-title' 			=> $jsLang == 1 ? 'Mapa Dirección' : 'Map Select',
+					'@maplaunch-text' 			=> $jsLang == 1 ? 'Seleccione una de las opciones' : 'Select one of options below',
 					//Check disabled
-					'@disabled-text' 			=> array('Este contenido esta deshabilitado por el momento.', 'Este contenido esta deshabilitado por el momento.')[$jsLang],
+					'@disabled-text' 			=> $jsLang == 1 ? 'Este contenido esta deshabilitado por el momento.' : 'This content is currently disabled.',
 					//Lightgallery
-					'@lgtitle-prev' 			=> array('Loading previous page ...', 'Cargando página anterior ...')[$jsLang],
-					'@lgtitle-next' 			=> array('Loading next page ...', 'Cargando siguiente página ...')[$jsLang],
+					'@lgtitle-prev' 			=> $jsLang == 1 ? 'Cargando página anterior ...' : 'Loading previous page ...',
+					'@lgtitle-next' 			=> $jsLang == 1 ? 'Cargando siguiente página ...' : 'Loading next page ...',
 				);
 
 $jsKey = array_keys($jsVariables);
