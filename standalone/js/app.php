fc1005b96c0b1f4584f0548aeaff50d0f93ed730
TriForceX <triforce@gznetwork.com>
2017-08-25 13:29:18 -0300

    [standalone + wordpress] JS language improvement

--- standalone/js/app.php
+++ standalone/js/app.php
@@ -19,9 +19,13 @@ class php extends utilities\php { }
 
 //php::get_error('warning');
 
+$jsSeparate = isset($_GET['separate']) ? 1 : 0;
+$jsLanguage = isset($_GET['language']) ? $_GET['language'] : 0;
+
 function jsGenerate()
 {
-	$jsLang = $_GET['lang']; //0 = English, 1 = Spanish
+	global $jsLanguage;
+	
 	$jsMinify = true;
 	$jsBuffer = '';
 	$jsUrl = php::get_main_url('/js');
@@ -45,30 +49,30 @@ function jsGenerate()
 						'@screen-widescreen' 	=> '1200', 
 						'@screen-full-hd' 		=> '1920', 
 						//Form Validation
-						'@validate-title' 			=> $jsLang == 1 ? 'Alerta Formulario' : 'Form Alert', 
-						'@validate-normal' 			=> $jsLang == 1 ? 'Por favor complete los campos.' : 'Please fill the fields.', 
-						'@validate-number'	 		=> $jsLang == 1 ? 'Por favor escriba un número válido.' : 'Please type a valid number.', 
-						'@validate-tel' 			=> $jsLang == 1 ? 'Por favor escriba un teléfono válido.' : 'Please type a phone number.', 
-						'@validate-pass' 			=> $jsLang == 1 ? 'Por favor complete su clave.' : 'Please fill your password.', 
-						'@validate-email' 			=> $jsLang == 1 ? 'Por favor escriba un E-Mail válido.' : 'Please type a correct E-Mail.',
-						'@validate-search' 			=> $jsLang == 1 ? 'Por favor complete el campo de busqueda.' : 'Please fill the search field.', 
-						'@validate-checkbox' 		=> $jsLang == 1 ? 'Por favor elija opciones.' : 'Please check an option.',
-						'@validate-radio' 			=> $jsLang == 1 ? 'Por favor elija una de las opciones.' : 'Please check one of the options.',
-						'@validate-textarea' 		=> $jsLang == 1 ? 'Por favor escriba un mensaje.' : 'Please write a message.',
-						'@validate-select' 			=> $jsLang == 1 ? 'Por favor seleccione una opción.' : 'Please select an option.',
-						'@validate-confirm-title' 	=> $jsLang == 1 ? 'Confirmar Formulario' : 'Form Confirm', 
-						'@validate-confirm-text' 	=> $jsLang == 1 ? '¿Estas seguro de que deseas enviar el formulario?' : 'Are you sure you want to send the previous info?', 
+						'@validate-title' 			=> $jsLanguage == 1 ? 'Alerta Formulario' : 'Form Alert', 
+						'@validate-normal' 			=> $jsLanguage == 1 ? 'Por favor complete los campos.' : 'Please fill the fields.', 
+						'@validate-number'	 		=> $jsLanguage == 1 ? 'Por favor escriba un número válido.' : 'Please type a valid number.', 
+						'@validate-tel' 			=> $jsLanguage == 1 ? 'Por favor escriba un teléfono válido.' : 'Please type a phone number.', 
+						'@validate-pass' 			=> $jsLanguage == 1 ? 'Por favor complete su clave.' : 'Please fill your password.', 
+						'@validate-email' 			=> $jsLanguage == 1 ? 'Por favor escriba un E-Mail válido.' : 'Please type a correct E-Mail.',
+						'@validate-search' 			=> $jsLanguage == 1 ? 'Por favor complete el campo de busqueda.' : 'Please fill the search field.', 
+						'@validate-checkbox' 		=> $jsLanguage == 1 ? 'Por favor elija opciones.' : 'Please check an option.',
+						'@validate-radio' 			=> $jsLanguage == 1 ? 'Por favor elija una de las opciones.' : 'Please check one of the options.',
+						'@validate-textarea' 		=> $jsLanguage == 1 ? 'Por favor escriba un mensaje.' : 'Please write a message.',
+						'@validate-select' 			=> $jsLanguage == 1 ? 'Por favor seleccione una opción.' : 'Please select an option.',
+						'@validate-confirm-title' 	=> $jsLanguage == 1 ? 'Confirmar Formulario' : 'Form Confirm', 
+						'@validate-confirm-text' 	=> $jsLanguage == 1 ? '¿Estas seguro de que deseas enviar el formulario?' : 'Are you sure you want to send the previous info?', 
 						//Video launch
-						'@videolaunch-title' 		=> $jsLang == 1 ? 'Compartir Enlace' : 'Share Link', 
-						'@videolaunch-text' 		=> $jsLang == 1 ? '¡El enlace ha sido copiado!' : 'The share link has been copied!',
+						'@videolaunch-title' 		=> $jsLanguage == 1 ? 'Compartir Enlace' : 'Share Link', 
+						'@videolaunch-text' 		=> $jsLanguage == 1 ? '¡El enlace ha sido copiado!' : 'The share link has been copied!',
 						//Map launch
-						'@maplaunch-title' 			=> $jsLang == 1 ? 'Mapa Dirección' : 'Map Select',
-						'@maplaunch-text' 			=> $jsLang == 1 ? 'Seleccione una de las opciones' : 'Select one of options below',
+						'@maplaunch-title' 			=> $jsLanguage == 1 ? 'Mapa Dirección' : 'Map Select',
+						'@maplaunch-text' 			=> $jsLanguage == 1 ? 'Seleccione una de las opciones' : 'Select one of options below',
 						//Check disabled
-						'@disabled-text' 			=> $jsLang == 1 ? 'Este contenido esta deshabilitado por el momento.' : 'This content is currently disabled.',
+						'@disabled-text' 			=> $jsLanguage == 1 ? 'Este contenido esta deshabilitado por el momento.' : 'This content is currently disabled.',
 						//Lightgallery
-						'@lgtitle-prev' 			=> $jsLang == 1 ? 'Cargando página anterior ...' : 'Loading previous page ...',
-						'@lgtitle-next' 			=> $jsLang == 1 ? 'Cargando siguiente página ...' : 'Loading next page ...',
+						'@lgtitle-prev' 			=> $jsLanguage == 1 ? 'Cargando página anterior ...' : 'Loading previous page ...',
+						'@lgtitle-next' 			=> $jsLanguage == 1 ? 'Cargando siguiente página ...' : 'Loading next page ...',
 					);
 
 	include('app-extras.php');
@@ -93,23 +97,15 @@ if(php::is_localhost())
 	{
 		unlink('app.js');
 	}
-	else if(file_exists('app-0.js'))
+	if(file_exists('app-'.$jsLanguage.'.js'))
 	{
-		unlink('app-0.js');
-	}
-	else if(file_exists('app-1.js'))
-	{
-		unlink('app-1.js');
+		unlink('app-'.$jsLanguage.'.js');
 	}
 	
 	echo jsGenerate();
 }
 else
 {
-	//Generate separate files
-	$jsSeparate = $_GET['separate']; 
-	$jsLanguage = $_GET['lang'];
-	
 	if($jsSeparate)
 	{
 		if(!file_exists('app-'.$jsLanguage.'.js'))
