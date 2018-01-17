7adfff1ddba3750de6da9e0109d51d8cb15b3319
TriForceX <triforce@gznetwork.com>
2018-01-17 00:42:52 -0300

    [wordpress + standalone] updated custom paginator and custom date moved

--- wordpress/wp-content/themes/websitebase/functions.php
+++ wordpress/wp-content/themes/websitebase/functions.php
@@ -58,6 +58,53 @@ class php extends utilities\php
 		}
 	}
 	
+	//Get custom date format
+	public static function show_date($date = false, $format = 'Y-m-d', $lang = 'eng', $abbr = false){
+
+		if(!$date){
+			$date = date('Y-m-d');
+		}
+		
+		$newDate = strtotime($date);
+		$finalDate = date($format, $newDate);
+		$langSet = $lang == 'esp' ? 1 : 0;
+		$langAbbr = $abbr ? 1 : 0;
+
+		$langDays = array(
+						array(
+							array("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"),
+							array("Domingo","Lunes","Martes","Miércoles","Jueves","Viernes","Sábado"),
+						),
+						array(
+							array("Sun","Mon","Tue","Wed","Thu","Fri","Sat"),
+							array("Dom","Lun","Mar","Mié","Jue","Vie","Sáb"),
+						)
+					);
+		
+		$langMonths = array(
+							array(
+								array("January","February","March","April","May","June","July ","August","September","October","November","December"),
+								array("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre", "Diciembre"),
+							),
+							array(
+								array("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sept","Oct","Nov","Dec"),
+								array("Ene","Feb","Mar","Abr","May","Jun","Jul","Ago","Sept","Oct","Nov","Dic"),
+							)
+						);
+
+
+		for ($day = 0; $day < 7; $day++)
+		{
+			$finalDate = str_replace($langDays[0][0][$day], $langDays[$langAbbr][$langSet][$day] , $finalDate);
+		}
+		for ($month = 0; $month < 12; $month++)
+		{
+			$finalDate = str_replace($langMonths[0][0][$month], $langMonths[$langAbbr][$langSet][$month], $finalDate);
+		}
+		
+		return $finalDate;
+	}
+	
 	//Get video embed url
 	public static function get_embed_video($url,$autoplay = false)
 	{
@@ -102,12 +149,12 @@ class php extends utilities\php
 	}
 
 	//Custom paginator
-	public static function custom_paginator($offset, $limit, $totalnum, $customclass, $customLeft = '&laquo;', $customRight = '&laquo;', $append = false){
+	public static function custom_paginator($offset, $limit, $totalnum, $customclass, $customLeft = '&laquo;', $customRight = '&laquo;', $append = false, $parentDiv = false){
 
 		if($append == false){
 			$append = get_bloginfo('url').'/'.get_query_var('post_type').'/'.get_the_slug($post->ID).'/?';
 		}
-
+		
 		if ($totalnum > $limit)
 		{
 			$pages = intval($totalnum / $limit);
@@ -136,6 +183,9 @@ class php extends utilities\php
 				$pageNext = $_GET['pag'];
 				$pageNumNext = $_GET['num'];
 			}
+			if($parentDiv != false){
+				echo '<div class="'.$parentDiv.'">';
+			}
 			echo '<div class="JSpaginator '.$customclass.'"><div class="JSpageItems">';
 			echo '<a class="JSpagePrev" href="'.$append.'pag='.$pagePrev.'&num='.$pageNumPrev.'">'.$customLeft.'</a>';	
 				for ($i = 1; $i <= $pages; $i++) {  // loop thru 
@@ -153,6 +203,9 @@ class php extends utilities\php
 				}
 			echo '<a class="JSpageNext" href="'.$append.'pag='.$pageNext.'&num='.$pageNumNext.'">'.$customRight.'</a>';
 			echo '</div></div>';
+			if($parentDiv != false){
+				echo '</div>';
+			}
 		}
 		return;
 	}
