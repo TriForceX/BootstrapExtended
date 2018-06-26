28af452997bfd6ed9eea0fa0f0bcfd2e0909f35c
TriForceX <triforce@gznetwork.com>
2018-06-26 18:07:14 -0400

    [wordpress + standalone] updated extra CSS & JS examples

--- standalone/css/style-extras.php
+++ standalone/css/style-extras.php
@@ -1,9 +1,11 @@
 <?php
 //Extras
 $css_extra['file'] = array(
-						  'extras/example.css',
-						  );
+						'extras/example.css',
+						//'extras/another.css',
+						);
 $css_extra['vars'] = array(
-						  //'@color-custom'	=> '#ffffff',
-						  );
+						//'@color-custom'	=> '#FFFFFF',
+						//'@color-another'	=> '#000000',
+						);
 ?>
--- standalone/js/app-extras.php
+++ standalone/js/app-extras.php
@@ -1,9 +1,11 @@
 <?php
 //Extras
 $js_extra['file'] = array(
-						 'extras/example.js',
-						 );
+						'extras/example.js',
+						//'extras/another.js',
+						);
 $js_extra['vars'] = array(
-						 //'@color-custom'	=> '#ffffff',
-						 );
+						//'@color-custom'	=> '#FFFFFF',
+						//'@color-another'	=> '#000000',
+						);
 ?>
