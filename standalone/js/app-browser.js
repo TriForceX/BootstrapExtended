6565322c6135ab065a968ad52364b8d6b8716a0a
TriForceX <triforce@gznetwork.com>
2018-12-04 15:43:28 -0300

    [bootstrap4] updated browser detection

--- standalone/js/app-browser.js
+++ standalone/js/app-browser.js
@@ -4,103 +4,102 @@
 function JSoldBrowserScreen()
 {
 	// Create style element
-	var JSoldBrowserCSS = document.createElement('style');
+	var css = document.createElement('style');
 
 	// Get main URL from SRC
-	var JSoldBrowserNUM = document.getElementsByTagName('script').length - 1;
-	var JSoldBrowserURL = document.getElementsByTagName('script')[JSoldBrowserNUM].getAttribute('src').replace(/\/js\/app-browser.js/g,'');
+	var index = document.getElementsByTagName('script').length - 1;
+	var url = document.getElementsByTagName('script')[index].getAttribute('src').replace(/\/js\/app-browser.js/g,'');
 
 	// Set style code
-	var JSoldBrowserStyle = 'body {										'+
-							'	background: white !important;			'+
-							'}											'+
-							'body *{									'+
-							'	visibility: hidden !important; 			'+
-							'}											'+
-							'.JSoldBrowserElem {						'+
-							'	background-color: white;				'+
-							'	position: fixed;						'+
-							'	display: table !important;				'+
-							'	visibility: visible !important;			'+
-							'	z-index: 9999;							'+
-							'	top: 0px;								'+
-							'	left: 0px;								'+
-							'	width: 100%;							'+
-							'	height: 100%;							'+
-							'}											'+
-							'.JSoldBrowserElem * {						'+
-							'	visibility: visible !important;			'+
-							'}											'+
-							'.JSoldBrowserElem > div {					'+
-							'	display: table-cell !important;			'+
-							'	vertical-align: middle;					'+
-							'	text-align: center;						'+
-							'	padding: 15px;							'+
-							'}											'+
-							'.JSoldBrowserElem > div > a {				'+
-							'	margin: 0px 5px 10px 5px;				'+
-							'	display: inline-block;					'+
-							'	position: relative;						'+
-							'	color: black;							'+
-							'}											'+
-							'.JSoldBrowserElem > div > a > img {		'+
-							'	width: 60px;							'+
-							'	height: 60px;							'+
-							'}											'+
-							'.JSoldBrowserElem > div > a > span {		'+
-							'	width: 60px;							'+
-							'	height: 70px;							'+
-							'	font-size: 11px;						'+
-							'	line-height: 13px;						'+
-							'	position: absolute;						'+
-							'	z-index: 10;							'+
-							'	background: white;						'+
-							'	padding: 5px 0px;						'+
-							'	display: none;							'+
-							'}											'+
-							'.JSoldBrowserElem > div > a:hover > span {	'+
-							'	display: block;							'+
-							'}											';
+	var style = 'body {										'+
+				'	background: white !important;			'+
+				'}											'+
+				'body *{									'+
+				'	visibility: hidden !important; 			'+
+				'}											'+
+				'.JSoldBrowserElem {						'+
+				'	background-color: white;				'+
+				'	position: fixed;						'+
+				'	display: table !important;				'+
+				'	visibility: visible !important;			'+
+				'	z-index: 9999;							'+
+				'	top: 0px;								'+
+				'	left: 0px;								'+
+				'	width: 100%;							'+
+				'	height: 100%;							'+
+				'}											'+
+				'.JSoldBrowserElem * {						'+
+				'	visibility: visible !important;			'+
+				'}											'+
+				'.JSoldBrowserElem > div {					'+
+				'	display: table-cell !important;			'+
+				'	vertical-align: middle;					'+
+				'	text-align: center;						'+
+				'	padding: 15px;							'+
+				'}											'+
+				'.JSoldBrowserElem > div > a {				'+
+				'	margin: 0px 5px 10px 5px;				'+
+				'	display: inline-block;					'+
+				'	position: relative;						'+
+				'	color: black;							'+
+				'}											'+
+				'.JSoldBrowserElem > div > a > img {		'+
+				'	width: 60px;							'+
+				'	height: 60px;							'+
+				'}											'+
+				'.JSoldBrowserElem > div > a > span {		'+
+				'	width: 60px;							'+
+				'	height: 70px;							'+
+				'	font-size: 11px;						'+
+				'	line-height: 13px;						'+
+				'	position: absolute;						'+
+				'	z-index: 10;							'+
+				'	background: white;						'+
+				'	padding: 5px 0px;						'+
+				'	display: none;							'+
+				'}											'+
+				'.JSoldBrowserElem > div > a:hover > span {	'+
+				'	display: block;							'+
+				'}											';
 
 	// Set style type
-	JSoldBrowserCSS.type = 'text/css';
+	css.type = 'text/css';
 
-	// Set to IExplorer or other browsers
-	if(JSoldBrowserCSS.styleSheet) JSoldBrowserCSS.styleSheet.cssText = JSoldBrowserStyle;
-	else JSoldBrowserCSS.appendChild(document.createTextNode(JSoldBrowserStyle));
+	// Set to IE or other browsers
+	if(css.styleSheet) css.styleSheet.cssText = style;
+	else css.appendChild(document.createTextNode(style));
 
 	// Append to head
-	document.getElementsByTagName('head')[0].appendChild(JSoldBrowserCSS);
+	document.getElementsByTagName('head')[0].appendChild(css);
 
-	// Append when body loads
-	window.onload = function(){
-		// Set browser screen element
-		var JSoldBrowserElem = '<div class="JSoldBrowserElem">'+
-								'	<div>'+
-								'		<h1>Update your browser</h1>'+
-								'		<p>You are using an old browser, please update.</p>'+
-								'		<a target="_blank" href="https://www.mozilla.org/firefox/new">'+
-								'			<img src="'+JSoldBrowserURL+'/img/base/browser/firefox.png">'+
-								'			<span>Mozilla Firefox</span>'+
-								'		</a>'+
-								'		<a target="_blank" href="https://www.google.com/chrome">'+
-								'			<img src="'+JSoldBrowserURL+'/img/base/browser/chrome.png">'+
-								'			<span>Google Chrome</span>'+
-								'		</a>'+
-								'		<a target="_blank" href="https://www.opera.com/download">'+
-								'			<img src="'+JSoldBrowserURL+'/img/base/browser/opera.png">'+
-								'			<span>Opera Browser</span>'+
-								'		</a>'+
-								'		<a target="_blank" href="https://www.microsoft.com/en-us/download/internet-explorer-11-for-windows-7-details.aspx">'+
-								'			<img src="'+JSoldBrowserURL+'/img/base/browser/iexplorer.png">'+
-								'			<span>Internet Explorer 11</span>'+
-								'		</a>'+
-								'	</div>'+
-								'</div>';
-		
-		// Append element to body
-		document.body.innerHTML += JSoldBrowserElem;
-	};
+	// Set browser screen element
+	var htmlElems = '<div>'+
+					'	<h1>Update your browser</h1>'+
+					'	<p>You are using an old browser, please update.</p>'+
+					'	<a target="_blank" href="https://www.mozilla.org/firefox/new">'+
+					'		<img src="'+url+'/img/base/browser/firefox.png">'+
+					'		<span>Mozilla Firefox</span>'+
+					'	</a>'+
+					'	<a target="_blank" href="https://www.google.com/chrome">'+
+					'		<img src="'+url+'/img/base/browser/chrome.png">'+
+					'		<span>Google Chrome</span>'+
+					'	</a>'+
+					'	<a target="_blank" href="https://www.opera.com/download">'+
+					'		<img src="'+url+'/img/base/browser/opera.png">'+
+					'		<span>Opera Browser</span>'+
+					'	</a>'+
+					'	<a target="_blank" href="https://www.microsoft.com/en-us/download/internet-explorer-11-for-windows-7-details.aspx">'+
+					'		<img src="'+url+'/img/base/browser/iexplorer.png">'+
+					'		<span>Internet Explorer 11</span>'+
+					'	</a>'+
+					'</div>';
+	
+	// Append to html
+	var elem = document.getElementsByTagName('html')[0];
+	var content = document.createElement('div');
+	content.className = 'JSoldBrowserElem';
+	content.innerHTML = htmlElems;
+	elem.firstChild.parentNode.insertBefore(content, elem.firstChild.nextSibling);
 }
 
 // Check browser
@@ -140,7 +139,21 @@ function JSoldBrowserCheck(system)
 // Do check version
 if((JSoldBrowserCheck('iexplorer') > -1 && JSoldBrowserCheck('iexplorer') <= 10.0) || (JSoldBrowserCheck('ios') > -1 && JSoldBrowserCheck('ios') < 7))
 {
+	// Stop page loading
+	if(window.stop) 
+	{ 
+		window.stop();
+	}
+	else if(document.execCommand)
+	{ 
+		document.execCommand('Stop');
+	}
+
+	// Load update screen
 	JSoldBrowserScreen();
+	
+	// Throw an error
+	throw new Error('You are using an old browser, please update.');
 }
 		
 /* ================================================= BASE BROWSER ================================================= */
