b808b0f71657895fb2228eabd03f305086782462
TriForceX <triforce@gznetwork.com>
2019-01-09 16:59:03 -0300

    [all] changed theme js event quotes

--- standalone/js/app-theme.js
+++ standalone/js/app-theme.js
@@ -14,7 +14,7 @@ $(document).ready(function(){
 
 });
 
-$(window).bind("load", function(){
+$(window).bind('load', function(){
 
 /* ================================================= THEME WINDOWS LOAD ================================================= */
 	
@@ -24,7 +24,7 @@ $(window).bind("load", function(){
 
 });
 
-$(document).on("JSresponsiveCode", function(event, bodyWidth, bodyHeight, bodyOrientation, bodyScreen){
+$(document).on('JSresponsiveCode', function(event, bodyWidth, bodyHeight, bodyOrientation, bodyScreen){
 
 /* ================================================= THEME RESPONSIVE CODE ================================================= */
 	
