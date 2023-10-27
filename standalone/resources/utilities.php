66f550b45ea282dbbc589f895fa7dfa44d223f9d
TriForceX <triforce@gznetwork.com>
2023-10-27 00:46:01 -0300

    Fix missing http build url function and improved css & js build

--- standalone/resources/utilities.php
+++ standalone/resources/utilities.php
@@ -31,6 +31,19 @@ use MatthiasMullie\PathConverter\Converter;
 
 class php
 {
+	// URL constants as defined in the PHP Manual under "Constants usable with http_build_url()".
+    const HTTP_URL_REPLACE = 1;
+    const HTTP_URL_JOIN_PATH = 2;
+    const HTTP_URL_JOIN_QUERY = 4;
+    const HTTP_URL_STRIP_USER = 8;
+    const HTTP_URL_STRIP_PASS = 16;
+    const HTTP_URL_STRIP_AUTH = 32;
+    const HTTP_URL_STRIP_PORT = 64;
+    const HTTP_URL_STRIP_PATH = 128;
+    const HTTP_URL_STRIP_QUERY = 256;
+    const HTTP_URL_STRIP_FRAGMENT = 512;
+    const HTTP_URL_STRIP_ALL = 1024;
+
 	// Set default timezone
 	public static function default_timezone()
 	{
@@ -230,6 +243,8 @@ class php
 
 			file_put_contents($local.'/'.$type.'/'.$final, $info.$content);
 
+			if(!self::is_localhost()) self::get_build($local."/".$type, true);
+
 			if($type == 'css')
 			{
 				echo '<link href="'.$url.'/css/'.$final.'" rel="stylesheet">'."\n";
@@ -257,6 +272,8 @@ class php
 				}
 
 				file_put_contents($local.'/'.($minify ? $file_min : $file_dist), $info.$content);
+
+				if(!self::is_localhost()) self::get_build($local."/".$type, true);
 
 				if($type == 'css')
 				{
@@ -300,13 +317,15 @@ class php
 				}
 				else
 				{
+					$file_build = self::get_build($local."/".$type, false);
+
 					if($type == 'css')
 					{
-						echo '<link href="'.$url.'/css/'.$final.'" rel="stylesheet">'."\n";
+						echo '<link href="'.$url.'/css/'.$final.'?build='.$file_build.'" rel="stylesheet">'."\n";
 					}
 					else
 					{
-						echo '<script src="'.$url.'/js/'.$final.'"></script>'."\n";
+						echo '<script src="'.$url.'/js/'.$final.'?build='.$file_build.'"></script>'."\n";
 					}
 				}
 			}
@@ -337,18 +356,29 @@ class php
 					{
 						$file_min = str_replace('.'.$type, '.min.'.$type, $file);
 						$file_dist = str_replace('.'.$type, '.dist.'.$type, $file);
+						$file_build = self::get_build($local."/".$type, false);
 						
 						if($type == 'css')
 						{
-							echo '<link href="'.$url.'/'.($minify ? $file_min : $file_dist).'" rel="stylesheet">'."\n".($file === end($data['file']) ? '' : "\t");
+							echo '<link href="'.$url.'/'.($minify ? $file_min : $file_dist).'?build='.$file_build.'" rel="stylesheet">'."\n".($file === end($data['file']) ? '' : "\t");
 						}
 						else
 						{
-							echo '<script src="'.$url.'/'.($minify ? $file_min : $file_dist).'"></script>'."\n";
+							echo '<script src="'.$url.'/'.($minify ? $file_min : $file_dist).'?build='.$file_build.'"></script>'."\n";
 						}
 					}
 				}
 			}
+		}
+	}
+
+	public static function get_build($path, $refresh)
+	{
+		if(!file_exists($path."/last-build.txt") || $refresh) {
+			file_put_contents($path."/last-build.txt", date('dmYHis'));
+			return date('dmYHis');
+		} else {
+			return file_get_contents($path."/last-build.txt");
 		}
 	}
 	
@@ -558,7 +588,7 @@ class php
 		if($remove != null){
 			$domain = str_replace($remove,'',$domain);
 		}
-		if(isset($websitebase['custom_main_url']) && $websitebase['custom_main_url'] != false){
+		if($websitebase['custom_main_url'] != false){
 			$domain = $websitebase['custom_main_url'];
 		}
 		return rtrim($domain, '/');
@@ -624,6 +654,112 @@ class php
             return $var;
         }
         return $default;
+    }
+
+	// Build a URL. The parts of the second URL will be merged into the first according to the flags argument.
+    public static function http_build_url($url, $parts = array(), $flags = self::HTTP_URL_REPLACE, &$new_url = array())
+    {
+        if (!is_array($url)) $url = parse_url($url);
+        if (!is_array($parts)) $parts = parse_url($parts);
+
+        if (!isset($url['query']) && !is_string($url['query'])) $url['query'] = null;
+        if (!isset($parts['query']) && !is_string($parts['query'])) $parts['query'] = null;
+
+        $keys = array('user', 'pass', 'port', 'path', 'query', 'fragment');
+
+        // HTTP_URL_STRIP_ALL and HTTP_URL_STRIP_AUTH cover several other flags.
+        if ($flags & self::HTTP_URL_STRIP_ALL) {
+            $flags |= self::HTTP_URL_STRIP_USER | self::HTTP_URL_STRIP_PASS | self::HTTP_URL_STRIP_PORT | self::HTTP_URL_STRIP_PATH | self::HTTP_URL_STRIP_QUERY | self::HTTP_URL_STRIP_FRAGMENT;
+        } elseif ($flags & self::HTTP_URL_STRIP_AUTH) {
+            $flags |= self::HTTP_URL_STRIP_USER | self::HTTP_URL_STRIP_PASS;
+        }
+
+        // Schema and host are alwasy replaced
+        foreach (array('scheme', 'host') as $part) {
+            if (isset($parts[$part])) {
+                $url[$part] = $parts[$part];
+            }
+        }
+
+        if ($flags & self::HTTP_URL_REPLACE) {
+            foreach ($keys as $key) {
+                if (isset($parts[$key])) {
+                    $url[$key] = $parts[$key];
+                }
+            }
+        } else {
+            if (isset($parts['path']) && ($flags & self::HTTP_URL_JOIN_PATH)) {
+                if (isset($url['path']) && substr($parts['path'], 0, 1) !== '/') {
+                    $url['path'] = rtrim(str_replace(basename($url['path']), '', $url['path']), '/') . '/' . ltrim($parts['path'], '/');
+                } else {
+                    $url['path'] = $parts['path'];
+                }
+            }
+
+            if (isset($parts['query']) && ($flags & self::HTTP_URL_JOIN_QUERY)) {
+                if (isset($url['query'])) {
+                    parse_str($url['query'], $url_query);
+                    parse_str($parts['query'], $parts_query);
+
+                    $url['query'] = http_build_query(array_replace_recursive($url_query, $parts_query));
+                } else {
+                    $url['query'] = $parts['query'];
+                }
+            }
+        }
+
+        if (isset($url['path']) && substr($url['path'], 0, 1) !== '/') {
+            $url['path'] = '/' . $url['path'];
+        }
+
+        foreach ($keys as $key) {
+            $strip = 'HTTP_URL_STRIP_' . strtoupper($key);
+            if ($flags & constant('utilphp\\util::' . $strip)) {
+                unset($url[$key]);
+            }
+        }
+
+        $parsed_string = '';
+
+        if (isset($url['scheme'])) {
+            $parsed_string .= $url['scheme'] . '://';
+        }
+
+        if (isset($url['user'])) {
+            $parsed_string .= $url['user'];
+
+            if (isset($url['pass'])) {
+                $parsed_string .= ':' . $url['pass'];
+            }
+
+            $parsed_string .= '@';
+        }
+
+        if (isset($url['host'])) {
+            $parsed_string .= $url['host'];
+        }
+
+        if (isset($url['port'])) {
+            $parsed_string .= ':' . $url['port'];
+        }
+
+        if (!empty($url['path'])) {
+            $parsed_string .= $url['path'];
+        } else {
+            $parsed_string .= '/';
+        }
+
+        if (isset($url['query'])) {
+            $parsed_string .= '?' . $url['query'];
+        }
+
+        if (isset($url['fragment'])) {
+            $parsed_string .= '#' . $url['fragment'];
+        }
+
+        $new_url = $url;
+
+        return $parsed_string;
     }
 	
 	// Add or remove query arguments to the URL
